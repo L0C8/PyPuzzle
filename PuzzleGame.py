b6ba9d1c2163c0f985f5bb745f7413267a8e5480
L0C8 <TimB02@protonmail.com>
2025-03-26 19:34:48 +0100

    added player and movement

--- PuzzleGame.py
+++ PuzzleGame.py
@@ -3,14 +3,49 @@ import pygame
 class PuzzleGame:
     def __init__(self, screen):
         self.screen = screen
-        self.font = pygame.font.SysFont(None, 36)
-        self.text = self.font.render("testing!", True, (255, 255, 255))
-        self.text_rect = self.text.get_rect(center=(screen.get_width() // 2, screen.get_height() // 2))
+        self.rows = 10
+        self.cols = 15
+        self.grid = [[' ' for _ in range(self.cols)] for _ in range(self.rows)]
+        
+        self.player_symbol = '@'
+        self.player_pos = [1, 1]
+
+        self.font = pygame.font.SysFont("Courier New", 24)
+        self.cell_width = screen.get_width() // self.cols
+        self.cell_height = screen.get_height() // self.rows
+
+        self.place_player()
+
+    def place_player(self):
+        for row in range(self.rows):
+            for col in range(self.cols):
+                if self.grid[row][col] == self.player_symbol:
+                    self.grid[row][col] = ' '
+
+        r, c = self.player_pos
+        self.grid[r][c] = self.player_symbol
 
     def handle_event(self, event):
-        pass  
+        if event.type == pygame.KEYDOWN:
+            r, c = self.player_pos
+            if event.key == pygame.K_UP and r > 0:
+                self.player_pos[0] -= 1
+            elif event.key == pygame.K_DOWN and r < self.rows - 1:
+                self.player_pos[0] += 1
+            elif event.key == pygame.K_LEFT and c > 0:
+                self.player_pos[1] -= 1
+            elif event.key == pygame.K_RIGHT and c < self.cols - 1:
+                self.player_pos[1] += 1
+            self.place_player()
 
     def update(self):
         pass  
+    
     def draw(self):
-        self.screen.blit(self.text, self.text_rect)
+        for row in range(self.rows):
+            for col in range(self.cols):
+                char = self.grid[row][col]
+                text = self.font.render(char, True, (255, 255, 255))
+                x = col * self.cell_width + 8
+                y = row * self.cell_height + 4
+                self.screen.blit(text, (x, y))
